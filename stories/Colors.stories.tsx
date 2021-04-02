bea4a1e45357289c4e709d3d83459f9631aeccc4
Artur Ivanov <arthur.ivanov.art@gmail.com>
2021-04-02 16:35:24 +0200

    figma iFrame minor fixes

--- stories/Colors.stories.tsx
+++ stories/Colors.stories.tsx
@@ -3,9 +3,9 @@ import { storiesOf } from '@storybook/react';
 
 const Neutral = () => {
     return (
-        <>
+        <div className="atlantum-full-view">
             <iframe
-                style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
+                style={{ borderRadius: '4px', border: 'none' }}
                 width="100%"
                 height="500"
                 src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F5CdA4Nbd80vV0fy43Or0kZ%2FAtlantum-Examples%3Fnode-id%3D1%253A190"
@@ -18,15 +18,15 @@ const Neutral = () => {
                 src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F5CdA4Nbd80vV0fy43Or0kZ%2FAtlantum-Examples%3Fnode-id%3D14%253A181"
                 allowFullScreen
             />
-        </>
+        </div>
     );
 };
 
 const Primary = () => {
     return (
-        <>
+        <div className="atlantum-full-view">
             <iframe
-                style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
+                style={{ borderRadius: '4px', border: 'none' }}
                 width="100%"
                 height="500"
                 src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F5CdA4Nbd80vV0fy43Or0kZ%2FAtlantum-Examples%3Fnode-id%3D1%253A191"
@@ -39,14 +39,14 @@ const Primary = () => {
                 src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F5CdA4Nbd80vV0fy43Or0kZ%2FAtlantum-Examples%3Fnode-id%3D14%253A213"
                 allowFullScreen
             />
-        </>
+        </div>
     );
 };
 const Danger = () => {
     return (
-        <>
+        <div className="atlantum-full-view">
             <iframe
-                style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
+                style={{ borderRadius: '4px', border: 'none' }}
                 width="100%"
                 height="500"
                 src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F5CdA4Nbd80vV0fy43Or0kZ%2FAtlantum-Examples%3Fnode-id%3D1%253A224"
@@ -59,15 +59,15 @@ const Danger = () => {
                 src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F5CdA4Nbd80vV0fy43Or0kZ%2FAtlantum-Examples%3Fnode-id%3D14%253A257"
                 allowFullScreen
             />
-        </>
+        </div>
     );
 };
 
 const Warning = () => {
     return (
-        <>
+        <div className="atlantum-full-view">
             <iframe
-                style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
+                style={{ borderRadius: '4px', border: 'none' }}
                 width="100%"
                 height="500"
                 src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F5CdA4Nbd80vV0fy43Or0kZ%2FAtlantum-Examples%3Fnode-id%3D1%253A225"
@@ -80,15 +80,15 @@ const Warning = () => {
                 src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F5CdA4Nbd80vV0fy43Or0kZ%2FAtlantum-Examples%3Fnode-id%3D14%253A278"
                 allowFullScreen
             />
-        </>
+        </div>
     );
 };
 
 const Success = () => {
     return (
-        <>
+        <div className="atlantum-full-view">
             <iframe
-                style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
+                style={{ borderRadius: '4px', border: 'none' }}
                 width="100%"
                 height="500"
                 src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F5CdA4Nbd80vV0fy43Or0kZ%2FAtlantum-Examples%3Fnode-id%3D1%253A226"
@@ -101,16 +101,16 @@ const Success = () => {
                 src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F5CdA4Nbd80vV0fy43Or0kZ%2FAtlantum-Examples%3Fnode-id%3D14%253A299"
                 allowFullScreen
             />
-        </>
+        </div>
     );
 };
 
 const Info = () => {
     return (
-        <>
+        <div className="atlantum-full-view">
             <iframe
-                style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
-                width="100%"
+                style={{ borderRadius: '4px', border: 'none' }}
+                width="90%"
                 height="500"
                 src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F5CdA4Nbd80vV0fy43Or0kZ%2FAtlantum-Examples%3Fnode-id%3D1%253A227"
                 allowFullScreen
@@ -122,7 +122,7 @@ const Info = () => {
                 src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F5CdA4Nbd80vV0fy43Or0kZ%2FAtlantum-Examples%3Fnode-id%3D14%253A320"
                 allowFullScreen
             />
-        </>
+        </div>
     );
 };
 
