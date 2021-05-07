9878b1ab38ffa47ae4e33e296532b33afd4ec88a
Artur Ivanov <arthur.ivanov.art@gmail.com>
2021-05-07 16:21:32 +0200

    updated index page for main website

--- website/src/components/hero-image.component.tsx
+++ website/src/components/hero-image.component.tsx
@@ -8,13 +8,18 @@ interface HeroImageProps {
 
 const StyledHeroImage = styled.section<HeroImageProps>`
     display: flex;
-    width: 100vh;
-    height: 100vm;
+    flex-direction: column;
+    width: 100%;
+    height: 100vh;
     align-items: center;
     justify-content: center;
-    background-image: url(${(props) => props.url});
+    background: url(${(props) => props.url}) no-repeat center;
+    background-size: auto;
 `;
 
-export const HeroImage: FC<HeroImageProps> = ({ url, children }) => {
+export const HeroImage: FC<HeroImageProps> = ({
+    url = 'https://ibb.co/hCyCBMZ',
+    children,
+}) => {
     return <StyledHeroImage url={url}>{children}</StyledHeroImage>;
 };
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,15 +1,18 @@
 import * as React from 'react';
 import { ThemeProvider } from 'styled-components';
-import GlobalStyles, { lightTheme } from '@atlantum/theme';
-import Heading from '@atlantum/heading';
+import { lightTheme, GlobalStyles } from '@atlantum/theme';
+import { Heading } from '@atlantum/heading';
+import { HeroImage } from '../components/hero-image.component';
 
 // markup
 const IndexPage = () => {
     return (
         <ThemeProvider theme={lightTheme}>
             <GlobalStyles />
-            <Heading as="h1">Atlantum</Heading>
-            <Heading as={'h5'}>Composable design system</Heading>
+            <HeroImage url={'https://i.ibb.co/zrXyZhd/cover.png'}>
+                <Heading as="h1">Atlantum</Heading>
+                <Heading as={'h5'}>Composable design system</Heading>
+            </HeroImage>
         </ThemeProvider>
     );
 };
